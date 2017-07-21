59c41ec6fb243d31a2e79811eb42a18751be7f5f
Maksym Odanets <maksym_odanets@epam.com>
2017-07-21 17:52:22 +0300

    - were added php doc comments

--- src/Blog/Controller/PostsController.php
+++ src/Blog/Controller/PostsController.php
@@ -4,11 +4,18 @@ namespace Blog\Controller;
 
 use Blog\CommandBus\Command\CreatePostCommand;
 use Blog\Repository\Criteria\PostCriteria;
+use Blog\Service\SearchEngine\SearchResult;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 
 class PostsController
 {
+    /**
+     * @param Request $request
+     * @param Application $app
+     *
+     * @return string
+     */
     public function createAction(Request $request, Application $app)
     {
         $data = $request->getContent();
@@ -18,6 +25,12 @@ class PostsController
         return 'ok';
     }
 
+    /**
+     * @param Request $request
+     * @param Application $app
+     *
+     * @return SearchResult
+     */
     public function searchAction(Request $request, Application $app)
     {
         $params = $request->query->all();
@@ -26,6 +39,12 @@ class PostsController
         return $app['search_engine']->match($criteria);
     }
 
+    /**
+     * @param $uuid
+     * @param Application $app
+     *
+     * @return SearchResult
+     */
     public function showAction($uuid, Application $app)
     {
         $criteria = new PostCriteria([
