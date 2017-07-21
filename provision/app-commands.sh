7eacd58100c829b63b051ba5264b34bb6eb4df8c
Maksym Odanets <maksym_odanets@epam.com>
2017-07-21 14:49:29 +0300

    - were added composer install and migrations commands to the provisions

--- provision/app-commands.sh
+++ provision/app-commands.sh
@@ -2,5 +2,11 @@
 
 printf "#################################\n### Running app scripts\n#################################\n"
 
+# Composer
+composer install -d ${PROJECT_DIR}
+
 # Creating DB
 php ${PROJECT_DIR}/app/console dbal:run-sql "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+# Migrations
+php ${PROJECT_DIR}/app/console migrations:migrate --no-interaction
